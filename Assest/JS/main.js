3c8e0efa42ea90ab8f904939eceb09b0acd47757
yassir rachad <yassirrachad1997@gmail.com>
2023-11-01 12:04:42 +0100

    details modifier en cours

---
+++ Assest/JS/main.js
@@ -0,0 +1,6 @@
+function changeImg(imgPath) {
+    let mainImg = document.querySelector('.mainImg');
+    mainImg.setAttribute('src', imgPath);
+    //setAttribute take two parameter 1 the name of the attrbute you want
+    //2 the new value of that attribute
+}
